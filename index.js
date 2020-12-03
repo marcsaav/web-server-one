27bb47a35ecabaaf2836fb566cd8a58cb316ae44
Marcos Saavedra <marcsaav96@gmail.com>
2020-12-03 13:09:03 -0500

    Built out index.js

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
+const express = require('express')
+const port = process.env.PORT || 4000
+
+const server = express()
+
+server.use(express.json())
+
 console.log('web 36 rocks')
 console.log(__dirname)
 console.log(process.env.USERNAME)
 console.log(process.env.PORT)
+
+server.use('/api/*', (__, res) => {
+    res
+        .json({ data: 'Marcos rules!'})
+})
+
+server.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+})
